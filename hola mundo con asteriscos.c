5434e2eb14623ed47d8b28013491265171a8b66d
IsraelAguirre <55851949+IsraelAguirre@users.noreply.github.com>
2019-10-03 14:24:28 -0500

    Add files via upload

---
+++ hola mundo con asteriscos.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+realizar un programa en C que imprima Hola Mundo usando *****
+
+*******************************************************************************/
+#include <stdio.h>// definicion de librerias 
+
+    
+    void main()
+    {
+    printf("*  *  ****  *     ****\t\t** **  *  *  **  *  **** ****\n");//en una linea se ecribio la palabra "hola mundo"
+    printf("****  *  *  *     ****\t\t* * *  *  *  * * *  *  * *  *\n");//utilizando tabuladores y espaciadores
+    printf("*  *  ****  ****  *  *\t\t*   *  ****  *  **  **** ****\n");//ademas de espacios 
+
+    }
+
